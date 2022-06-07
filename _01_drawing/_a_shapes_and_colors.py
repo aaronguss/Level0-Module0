01b8b5fef275fe078a5f97a1fb3f421a17db660f
aaronguss <aaronginfo@gmail.com>
2022-06-07 11:54:18 -0700

    Today I worked on learning how to make color and shapes and make a spiral using python

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -1,36 +1,56 @@
 import turtle
-
 if __name__ == '__main__':
     window = turtle.Screen()
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    tooty = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    tooty.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    tooty.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    tooty.color('green')
+    tooty.pencolor('blue')
     # Move your turtle forward using .forward(100)
+    for i in range(4):
+        print(i)
+        tooty.forward(100)
     # TEST    Did your turtle move forward?
 
     # Move your turtle left or right using .left(90) or .right(90)
-
+        tooty.left(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
+    tooty.goto(5, 8)
     # x=0 and y=0 is the center of the screen
 
     # Have your turtle draw a circle using .circle(radius, steps=30)
+    tooty.begin_fill()
+    tooty.circle(radius=250, steps=15)
     # TEST    Did your turtle draw a circle?
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
-    # and .end_fill() below
 
+    # and .end_fill() below
+    tooty.end_fill()
     # Draw 3 more shapes with different fill colors!
+    tooty.fillcolor('blue')
+    tooty.begin_fill()
+    tooty.circle(radius=35, steps=3)
+    tooty.end_fill()
+    tooty.fillcolor('pink')
+    tooty.begin_fill()
+    tooty.circle(radius=100, steps=5)
+    tooty.end_fill()
+    tooty.fillcolor('aquamarine')
+    tooty.begin_fill()
+    tooty.circle(radius=180, steps=8)
+    tooty.end_fill()
+
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,23 +14,25 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
+    tooty=turtle.Turtle()
     
     # This code sets our shape to a turtle
+    tooty.shape('turtle')
     
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    tooty.speed(10)
     # Set your turtle's color using .color('green')
-    
+    tooty.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(200000000000000000000000):
         # Set the turtle color to a random color
-
+        tooty.color(get_random_color())
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
+        tooty.forward(5*i)
         # Turn the turtle (360/7) degrees to the right
-         
+        tooty.right(360/7)
         # Change the turtle width to 'i' (the loop variable)
-        
+        tooty.width(i)
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
